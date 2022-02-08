d4683984314f7bdc3b37aa5a34569276b0ae1998
bogxoxl <BogdanXoxl>
2022-02-08 13:20:05 +0300

    add layout and init first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import React, {FC} from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import {GlobalStyles, Container, Content, BackgroundVideo} from "./index.styles";
+import MainPage from "./pages/MainPage";
+import Layout from "./components/Layout";
 
 const App:FC = () => {
-  return (
-    <>
-    </>
-  );
+    return (
+        <>
+            <Container>
+                <Content>
+                    <Routes>
+                        <Route index element={<MainPage/>}/>
+                        <Route element={<Layout/>}>
+                            <Route path="/form"/>
+                            <Route path="/final"/>
+                        </Route>
+                    </Routes>
+                </Content>
+                <BackgroundVideo autoPlay muted loop preload="auto">
+                    <source src="background.mp4" type="video/mp4"/>
+                </BackgroundVideo>
+            </Container>
+            <GlobalStyles/>
+        </>
+    );
 };
 
 export default App;
---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const MainPage:FC = () => {
+    return (
+        <div>
+            index page
+        </div>
+    );
+};
+
+export default MainPage;
